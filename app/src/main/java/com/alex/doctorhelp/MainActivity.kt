91b29ba4bb512ab26489622be86168abf40c577b
Alex <sturmmanngr@gmail.com>
2021-10-27 18:57:48 +0700

    выполнено задание со стихотворением

--- app/src/main/java/com/alex/doctorhelp/MainActivity.kt
+++ app/src/main/java/com/alex/doctorhelp/MainActivity.kt
@@ -2,8 +2,14 @@ package com.alex.doctorhelp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+
+private const val TAG = "строчка"
+private var flag: Int = 0
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +34,89 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        if (flag == 0) {
+            Log.d(TAG, "Ты видел деву на скале")
+
+        }
+        else {
+            Log.d(TAG, "И ветер бился и летал")
+
+        }
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        if (flag == 0) {
+            Log.d(TAG, "В одежде белой над волнами")
+
+        }
+        else {
+            Log.d(TAG, "С ее летучим покрывалом?")
+
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (flag == 0) {
+            Log.d(TAG, "Когда, бушуя в бурной мгле,")
+
+        }
+        else {
+            Log.d(TAG, "Прекрасно море в бурной мгле")
+
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (flag == 0) {
+            Log.d(TAG, "Играло море с берегами,")
+
+        }
+        else {
+            Log.d(TAG, "И небо в блесках без лазури;")
+
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (flag == 0) {
+            Log.d(TAG, "Когда луч молний озарял")
+
+        }
+        else {
+            Log.d(TAG, "Но верь мне: дева на скале")
+
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (flag == 0) {
+            Log.d(TAG, "Ее всечасно блеском алым")
+            flag = 1
+        }
+        else {
+            Log.d(TAG, "Прекрасней волн, небес и бури.")
+            flag = 0
+        }
+    }
+
+
+//    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+//        super.onSaveInstanceState(outState, outPersistentState)
+//        Log.d(TAG, "onSaveInstanceState")
+//    }
 
     fun setAlphabet(): MutableList<Char> {
         val alphabet = "abcdefghijklmnopqrstuvwxyz"
