c2a9586c6ee11f8a29e691717c9708b49dfa3fb3
Alex <sturmmanngr@gmail.com>
2021-11-08 15:15:50 +0700

    ДЗ со стилями доделано

--- app/src/main/java/com/alex/doctorhelp/NextActivityTest.kt
+++ app/src/main/java/com/alex/doctorhelp/NextActivityTest.kt
@@ -3,6 +3,8 @@ package com.alex.doctorhelp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NextActivityTest : AppCompatActivity() {
@@ -10,11 +12,13 @@ class NextActivityTest : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next_test)
 
-
+        val greenButton: Button = findViewById(R.id.greenButton)
         val fab: FloatingActionButton = findViewById(R.id.floating_action_button)
         val nextActIntent: Intent = Intent(this, practice_6_7_november::class.java)
 
-
+        greenButton.setOnClickListener{
+            Toast.makeText(applicationContext, "Вы нажали на кнопку!", Toast.LENGTH_SHORT).show()
+        }
 
         fab.setOnClickListener {
             startActivity(nextActIntent)
